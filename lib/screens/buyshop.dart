0ce76c5f8951d8f4fb65f89449a0fee5f1756bde
Kei - Theerawat <theerawat.kei@mail.kmutt.ac.th>
2023-05-22 20:53:33 +0700

    Add BuyShop

---
+++ lib/screens/buyshop.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class BuyShopRoute extends StatelessWidget {
+  const BuyShopRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold
+    (
+      body: BuyShopRoutePage()
+    );
+  }
+}
+
+class BuyShopRoutePage extends StatefulWidget {
+  const BuyShopRoutePage({super.key});
+
+  @override
+  State<BuyShopRoutePage> createState() => BuyShopRoutePageState();
+}
+
+class BuyShopRoutePageState extends State<BuyShopRoutePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+
+    );
+  }
+}
