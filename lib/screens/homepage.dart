ea3d6c4a62a87b53e00b8877828bcbccc66749e3
Kei - Theerawat <theerawat.kei@mail.kmutt.ac.th>
2023-05-22 15:11:01 +0700

    homepage

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -5,13 +5,74 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [Text("Homepage")],
-        ),
+    return const Scaffold
+    (
+      body: HomePageButton()
+    );
+  }
+}
+
+class HomePageButton extends StatefulWidget {
+  const HomePageButton({super.key});
+
+  @override
+  State<HomePageButton> createState() => HomePageButtonState();
+}
+
+class HomePageButtonState extends State<HomePageButton> {
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const SizedBox(
+            child: Center(
+              child:Text('Liberate Goods Shop'
+              ,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold)
+              )
+            )
+          ),
+          const SizedBox(
+            height: 250,
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: onPressedLogin,
+            child: const SizedBox(
+                width: 250,
+                child: Center(
+                  child: Text('Login')
+                ),
+            ),
+          ),
+          const SizedBox(height: 20,
+          ),
+          ElevatedButton(
+            style: style,
+            onPressed: onPressedSignUp,
+            child: const SizedBox(
+                width: 250,
+                child: Center(
+                  child: Text('Sign Up')
+                ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+  void onPressedLogin()
+  {
+    //Redirect here
+  }
+
+  void onPressedSignUp()
+  {
+    //Redirect here
+  }
