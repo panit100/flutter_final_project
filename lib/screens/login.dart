1f2684aee6f7b49272909a8f12a06781fe86159c
Kei - Theerawat <theerawat.kei@mail.kmutt.ac.th>
2023-05-22 20:38:28 +0700

    update login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -5,7 +5,10 @@ class LoginRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: LoginPageHeader());
+    return const Scaffold
+    (
+      body: LoginPageHeader()
+    );
   }
 }
 
@@ -17,104 +20,124 @@ class LoginPageHeader extends StatefulWidget {
 }
 
 class LoginPageHeaderState extends State<LoginPageHeader> {
-  bool isShow = false;
-  void onButtonPress() {
-    setState(() {
-      isShow = !isShow;
-    });
-  }
-
+    bool isShow = false;
+    String username = '';
+    String password = '';
+    String errorText = '';
+    void onButtonPress()
+    {
+      setState(() {
+        isShow = !isShow;
+      });
+    }
+    void onPasswordFieldChanged(String value)
+    {
+      setState(() {
+        password = value;
+      });
+    }
+    void onUsernameFieldChanged(String value)
+    {
+      setState(() {
+        username = value;
+      });
+    }
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(
-          height: 170,
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 30),
-          child: SizedBox(
+        children:[
+          const SizedBox(
+            height: 170,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 30),
+            child: SizedBox(
               width: 700,
-              child: Text(
-                'Welcome',
-                style: TextStyle(fontSize: 20),
-                textAlign: TextAlign.left,
-              )),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 30, top: 10),
-          child: SizedBox(
-              width: 700,
-              child: Text(
-                'Please Login or Sign up to our app',
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.left,
-              )),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 30),
-          child: Text(
-            'Username',
-            style: TextStyle(fontSize: 10),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30, right: 30),
-          child: TextFormField(
-            decoration: const InputDecoration(hintText: 'Enter Username'),
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        const Padding(
-          padding: EdgeInsets.only(left: 30),
-          child: Text(
-            'Password',
-            style: TextStyle(fontSize: 10),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 30, right: 30),
-          child: TextFormField(
-            obscureText: true,
-            decoration: const InputDecoration(
-              hintText: 'Enter Password',
+              child: Text('Welcome',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.left,)
             ),
           ),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        Center(
-          child: ElevatedButton(
-              onPressed: onButtonPress,
+          const Padding(
+            padding: EdgeInsets.only(left: 30,top: 10),
+            child: SizedBox(
+              width: 700,
+              child: Text('Please Login or Sign up to our app',
+              style: TextStyle(fontSize: 15),
+              textAlign: TextAlign.left,)
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 30),
+            child: Text('Username',
+            style: TextStyle(fontSize: 10),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30,right: 30),
+            child: TextFormField(
+              onChanged: (value) {
+                onUsernameFieldChanged(value);
+              },
+              decoration: const InputDecoration(
+              hintText: 'Enter Email'
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 30),
+            child: Text('Password',
+            style: TextStyle(fontSize: 10),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30,right: 30),
+            child: TextFormField(
+              onChanged: (value) {
+                onPasswordFieldChanged(value);
+              },
+              obscureText: true,
+              decoration: const InputDecoration(
+              hintText: 'Enter Password',
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Center(
+            child: ElevatedButton(
+              onPressed: (() {
+                onButtonPress();
+                //Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginRoute())); #Change LoginRoute to BuyShopRoute 
+                }
+              ),
               child: const SizedBox(
                 width: 200,
-                child: Text(
-                  'Login',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 25),
-                ),
-              )),
-        ),
-        Visibility(
+                child: Text('Login',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25),),
+                )
+            ),
+          ),
+          Visibility(
             visible: isShow,
-            child: const Center(
+            child: Center(
               child: Padding(
-                padding: EdgeInsets.only(left: 30, right: 30),
-                child: Text(
-                  'Error Text',
-                  textAlign: TextAlign.center,
+                padding: const EdgeInsets.only(left: 30,right: 30),
+                child: Text(errorText.toString(),
+                textAlign: TextAlign.center,
                 ),
               ),
-            ))
-      ],
-    );
+            )    
+          )
+        ],
+      );
   }
 }
